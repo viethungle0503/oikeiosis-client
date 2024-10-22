b4c9baabeb655180e65e235fa84a08f9a21cc69b
viethungle0503 <viethungitmi@gmail.com>
2024-10-22 10:25:40 +0700

    Allow mock axios to work even in production environment

--- src/mocks/axios.ts
+++ src/mocks/axios.ts
@@ -1,17 +1,15 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 
-if (process.env.NODE_ENV === 'development' || import.meta.env.VITE_SERVER_MODE === 'development') {
-  // Create a new instance of MockAdapter
-  const mock = new MockAdapter(axios);
+// Create a new instance of MockAdapter
+const mock = new MockAdapter(axios);
 
-  // Set a mock response for a GET request to /api/secured
-  mock.onGet('http://127.0.0.1:8000/api/secured').reply(200, {
-    message: 'Mocked response',
-    name: 'Mocked Name',
-    email: 'Mocked Email',
-    isAdmin: true
-  });
+// Set a mock response for a GET request to /api/secured
+mock.onGet('http://127.0.0.1:8000/api/secured').reply(200, {
+  message: 'Mocked response',
+  name: 'Mocked Name',
+  email: 'Mocked Email',
+  isAdmin: true
+});
 
-  mock.onAny().passThrough(); // Pass any non-mocked requests through
-}
+mock.onAny().passThrough(); // Pass any non-mocked requests through
